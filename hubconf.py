b6ff9aeeba2d4bbf14ead6f221eec929c5523604
W1lliaNBel0tt0 <101693364+W1lliaNBel0tt0@users.noreply.github.com>
2022-07-10 00:40:25 -0300

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -121,7 +121,7 @@ def yolov5x6(pretrained=True, channels=3, classes=80, autoshape=True, verbose=Tr
 
 
 if __name__ == '__main__':
-    model = _create(name='yolov5s', pretrained=True, channels=6, classes=80, autoshape=True, verbose=True)  # pretrained
+    model = _create(name='yolov5s', pretrained=True, channels=12, classes=80, autoshape=True, verbose=True)  # pretrained
     # model = custom(path='path/to/model.pt')  # custom
 
     # Verify inference
