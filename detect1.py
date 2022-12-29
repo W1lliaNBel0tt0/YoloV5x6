2ccf728ce757021492290d0f6540a75aaae5059f
W1lliaNBel0tt0 <101693364+W1lliaNBel0tt0@users.noreply.github.com>
2022-12-29 04:48:07 -0300

    test

--- detect1.py
+++ detect1.py
@@ -51,10 +51,10 @@ from utils.torch_utils import select_device, time_sync
 def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
         source=ROOT / 'data/images',  # file/dir/URL/glob, 0 for webcam
         data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
-        imgsz=(320, 320),  # inference size (height, width)
+        imgsz=(100, 100),  # inference size (height, width)
         conf_thres=0.25,  # confidence threshold
         iou_thres=0.45,  # NMS IOU threshold
-        max_det=1000,  # maximum detections per image
+        max_det=200,  # maximum detections per image
         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
         view_img=False,  # show results
         save_txt=False,  # save results to *.txt
