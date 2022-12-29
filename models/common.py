bd0ae55023d9dc92e1d35b5762cd82258ac1bc4e
W1lliaNBel0tt0 <101693364+W1lliaNBel0tt0@users.noreply.github.com>
2022-12-29 05:04:58 -0300

    test

--- models/common.py
+++ models/common.py
@@ -513,7 +513,7 @@ class AutoShape(nn.Module):
         return self
 
     @torch.no_grad()
-    def forward(self, imgs, size=320, augment=False, profile=False):
+    def forward(self, imgs, size=160, augment=False, profile=False):
         # Inference from various sources. For height=640, width=1280, RGB images example inputs are:
         #   file:       imgs = 'data/images/zidane.jpg'  # str or PosixPath
         #   URI:             = 'https://ultralytics.com/images/zidane.jpg'
